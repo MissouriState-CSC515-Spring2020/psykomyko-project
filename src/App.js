2ff2d87d23b2bb18473562c7708cdd2e1584b483
Michael McDonald <michaelmac730@gmail.com>
2020-02-05 18:49:16 -0600

    Added Bootstrap framework

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
+import NavBar from './components/NavBar'
+import {
+  Jumbotron     
+} from 'reactstrap';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends React.Component { 
 
-export default App;
+  render() {
+    return (
+      <div>
+        <NavBar />
+        <Jumbotron>
+          <h1 className="display-3">CSC515 School Project</h1>
+          <p className="lead">Welcome to my project!</p>
+          <hr className="my-2" />
+          <p>Checkpoint 2</p>
+          <p className="lead">
+          </p>
+        </Jumbotron>
+         
+
+      </div>
+    );
+  }  
+};
