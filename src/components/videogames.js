3ecd03c359f054db34638eb82bed61e3ed2b6ef9
Michael McDonald <michaelmac730@gmail.com>
2020-02-21 14:37:28 -0600

    Checkpoint-3 done

---
+++ src/components/videogames.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export default class VideoGames extends React.Component { 
+
+  render() {
+    return (
+      <div className = "main-container">
+          <head>
+            <meta charset="utf-8" />    
+            <meta name="viewport" content="width=device-width, initial-scale=2" />
+            <title>Video Games!</title> 
+          </head>
+          <body>
+            <div class="container">
+            <header>
+                <img src="Images/game_logo_collage_by_regnoart-dbjhp96.jpg"/>              
+                           
+            </header>  
+
+            </div> 
+           </body>        
+      </div>
+    );
+  }  
+};
